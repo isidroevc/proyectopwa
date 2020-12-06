5803db3e47135451ef955ce4d078a680bd6003be
Isidro Vasquez <isidroevc@gmail.com>
2020-12-06 17:18:14 -0600

    First commit

---
+++ src/main/java/com/isidroevc/proyectopwa/ProyectopwaApplication.java
@@ -0,0 +1,13 @@
+package com.isidroevc.proyectopwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectopwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectopwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isidroevc/proyectopwa/ProyectopwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isidroevc.proyectopwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
